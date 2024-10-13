609c9c600cbf2379b198415c1f1c2cc842a56dd3
EC2 Default User <kimikuma>
2024-10-13 11:58:19 +0000

    更新

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,34 @@
 class Book < ApplicationRecord
+
+  #アソシエーション
   belongs_to:user
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
 
+  #バリデーション
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  #いいね機能を一回のみの使用にするための設定
   def favorited_by?(user)
     favorites.exists?(user_id:user.id)
   end
+
+  #検索機能 条件分岐
+
+  def self.looks(search,word)
+    if search=="perfect_match"
+      @book=Book.where("title LIKE?","#{word}")
+    elsif search=="forward_match"
+      @book=Book.where("title LIKE?","#{word}%")
+    elsif search=="backward_match"
+      @book=Book.where("title LIKE?","%#{word}")
+    elsif search=="partial_match"
+      @book=Book.where("title LIKE?","%#{word}%")
+    else
+      @book=Book.all
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
     resources :book_comments,onle: [:create,:destroy]
   end
 
+  get "search"=>"searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
