eb3e21c3cc581503a46be642a809fc055f0d1aeb
EC2 Default User <kimikuma>
2024-10-14 12:32:31 +0000

    [final]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book=Book.find(params[:book_id])
+    @book=Book.find(params[:book_id])
     comment=current_user.book_comments.new(book_comment_params)
-    comment.book_id=book.id
+    comment.book_id=@book.id
     comment.save
     #redirect_to book_path(book.id)
   end
 
   def destroy
-    book=Book.find(params[:book_id])
-    BookComment.find(params[:id]).destroy
+
+　　comment=BookComment.find(params[:id])
+　　comment.destroy
+　  @book=Book.find(params[:book_id])
 
     #redirect_to book_path(params[:book_id])
   end
