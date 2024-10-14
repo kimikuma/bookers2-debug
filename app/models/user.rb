7f21fe45bb5fda5d56f10a7e01437b9670937327
EC2 Default User <kimikuma>
2024-10-14 01:25:56 +0000

    [final]search

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,17 +46,17 @@ class User < ApplicationRecord
   end
 
 
- def self.looks(search, word)
-    if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @user = User.where("name LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?","%#{word}%")
+ def self.looks(search,word)
+    if search=="perfect_match"
+      @user=User.where("name LIKE?","#{word}")
+    elsif search=="forward_match"
+      @user=User.where("name LIKE?","#{word}%")
+    elsif search=="backward_match"
+      @user=User.where("name LIKE?","%#{word}")
+    elsif search=="partial_match"
+      @user=User.where("name LIKE?","%#{word}%")
     else
-      @user = User.all
+      @user=User.all
     end
  end
 
